ccd65659b274ede55237477d45283ab13467783d
ARNAB DAS <raj713335@gmail.com>
2024-06-10 03:12:39 +0530

    :wrench: Adding keycloak security

--- src/main/java/com/example/keycloak_spring_boot/DemoController.java
+++ src/main/java/com/example/keycloak_spring_boot/DemoController.java
@@ -1,23 +1,28 @@
 package com.example.keycloak_spring_boot;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/demo")
+@RequestMapping("/api")
 public class DemoController {
 
+
     @GetMapping("/hello")
-    @PreAuthorize("hasRole('client_user')")
-    public String hello() {
-        return "Hello from SpringBoot & Keycloak";
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello");
     }
 
-    @GetMapping("/hello-2")
-    @PreAuthorize("hasRole('client_user')")
-    public String hello2() {
-        return "Hello from SpringBoot & Keycloak - ADMIN";
+    @GetMapping("/admin")
+    public ResponseEntity<String> sayHelloToAdmin() {
+        return ResponseEntity.ok("Hello Admin");
     }
+
+    @GetMapping("/user")
+    public ResponseEntity<String> sayHelloToUser() {
+        return ResponseEntity.ok("Hello User");
+    }
+
 }
